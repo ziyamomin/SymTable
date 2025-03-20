9df2bd8fa44fe8e7632bec287404bf7ad1cd9385
Ziya Momin <ziyamomin@gmail.com>
2025-03-19 23:28:43 -0400

    Implementation bare bones set up

--- symtablelist.c
+++ symtablelist.c
@@ -5,38 +5,54 @@ SymTable_T SymTable_new(void) {
 }
 
 void SymTable_free(SymTable_T oSymTable) {
+    assert(oSymTable != NULL);
 
 }
 
 size_t SymTable_getLength(SymTable_T oSymTable) {
-
+    assert(oSymTable != NULL);
 }
 
 int SymTable_put(SymTable_T oSymTable,
      const char *pcKey, const void *pvValue) {
-
+        assert(oSymTable != NULL);
+        assert(pcKey != NULL);
+        assert(pcValue != NULL);
      }
 
 void *SymTable_replace(SymTable_T oSymTable,
      const char *pcKey, const void *pvValue) {
+        assert(oSymTable != NULL);
+        assert(pcKey != NULL);
+        assert(pcValue != NULL);
 
      }
 
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey) {
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
 
 }
 
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey) {
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
 
 }
 
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey) {
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
 
 }
 
 void SymTable_map(SymTable_T oSymTable,
      void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
      const void *pvExtra) {
+        assert(oSymTable != NULL);
+        assert(pcKey != NULL);
+        assert(pcValue != NULL);
+        assert(pvExtra != NULL);
 
      }
 
